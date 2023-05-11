7f7b9d7063bf6bac9a9417ddca6cd9aed2fc16c9
cprajwowski <c.prajwowski.631@studms.ug.edu.pl>
2023-05-11 08:24:58 +0200

    Kolokwium!

--- lab07/src/shipsy.js
+++ lab07/src/shipsy.js
@@ -66,7 +66,7 @@ class Tile {
         this.x = x;
         this.y = y;
         this.grid = grid;
-        this.grid.ships === null ? this.grid.ships = [this] : this.grid.ships.push(this)
+        this.grid.ships === null ? this.grid.ships = [this] : this.grid.ships.push(this);
     }
 
     turn(strona){
@@ -88,16 +88,8 @@ class Tile {
   }
 
 class IceBreakerShip extends Ship{
-    direction = null; // string
-
-    x = null; // liczba całkowita
-
-    y = null; // liczba całkowita
-
-    grid = null; // obiekt klasy Grid
-
     constructor(x, y, grid){
-        super('N', x, y, grid);
+        super("N", x, y, grid);
     }
 
     turn(){
@@ -105,11 +97,24 @@ class IceBreakerShip extends Ship{
     }
 
     sail(){
-        (this.direction === 'N' ? ((this.grid.grid[this.x-1][this.y].type === 'WATER' || this.grid.grid[this.x-1][this.y].type === 'ICE') ? this.x = this.x - 1 : super.turn())
-        : this.direction === 'E' ? (this.grid.grid[this.x][this.y+1].type === 'WATER' || this.grid.grid[this.x][this.y+1].type === 'ICE'? this.y = this.y + 1 : super.turn())
-        : this.direction ===' S' ? (this.grid.grid[this.x+1][this.y].type === 'WATER' || this.grid.grid[this.x+1][this.y].type === 'ICE'? this.x = this.x + 1 : super.turn())
-        : ((this.grid.grid[this.x][this.y-1].type === 'WATER' || this.grid.grid[this.x][this.y-1].type === 'ICE') ? this.y = this.y - 1 : super.turn()));
+        this.direction === 'N'
+    ? this.grid.grid[this.x - 1][this.y].type === 'WATER' || this.grid.grid[this.x - 1][this.y].type === 'ICE'
+    ? this.x = this.x - 1
+    : console.log("Nie możemy!")
+    : this.direction === 'E'
+    ? this.grid.grid[this.x][this.y + 1].type === 'WATER' || this.grid.grid[this.x][this.y + 1].type === 'ICE'
+      ? this.y = this.y + 1
+      : console.log("Nie możemy!")
+    : this.direction === 'S'
+    ? this.grid.grid[this.x + 1][this.y].type === 'WATER' || this.grid.grid[this.x + 1][this.y].type === 'ICE'
+      ? this.x = this.x + 1
+      : console.log("Nie możemy!")
+    : this.grid.grid[this.x][this.y - 1].type === 'WATER' || this.grid.grid[this.x][this.y - 1].type === 'ICE'
+    ? this.y = this.y - 1
+    : console.log("Nie możemy!")
+        super.turn();
     }
+
 }
 
 // Przykład użycia:
@@ -117,13 +122,9 @@ const grid = new Grid();
 grid.generate(10, 10); // generuje planszę o wymiarach 10 x 10
 grid.getTile(2, 2).getTileInfo()// 2,2 LAND
 grid.generate(10, 10); // na nowo generuje planszę o wymiarach 10 x 5
-
 // Przykład użycia:
-const ship = new Ship("N", 5, 5 ,grid);
-
-
+const ship = new Ship("N", 5, 5 , grid);
 ship.turn("P"); // Statek obrócony o 90 st. w prawo
-
 ship.sail(); // Statek poruszył się o 1 kafelek na wschód
 console.log(ship.getShipInfo());
 ship.sail(); // Statek poruszył się o 1 kafelek na wschód
@@ -131,8 +132,7 @@ console.log(ship.getShipInfo());
 
 // Przykład użycia:
 const icebreaker = new IceBreakerShip(3, 5, grid);
-
-icebreaker.sail(); // Statek nacpotkał lądolód, przedarł się przez niego
-
+icebreaker.sail();
+console.log(icebreaker.getShipInfo());
 
 
